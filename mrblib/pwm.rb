dc138bfbf27f06d21803661a267b45c8619e581e
yuuu <okazaki@fusic.co.jp>
2023-08-06 07:13:56 +0900

    Fix typos and remove print.

--- mrblib/pwm.rb
+++ mrblib/pwm.rb
@@ -5,12 +5,10 @@ class PWM
     self.group = kwargs[:group] || ESP32::LEDC_LOW_SPEED_MODE
     self.channel = kwargs[:channel] || ESP32::LEDC_CHANNEL_0
     self.timer = kwargs[:timer] || ESP32::LEDC_TIMER_0
-    self.resolution = kwargs[resolution] || ESP32::LEDC_TIMER_14_BIT
+    self.resolution = kwargs[:resolution] || ESP32::LEDC_TIMER_14_BIT
 
     f = kwargs[:freq] || kwargs[:frequency] || 1000
     d = kwargs[:duty] || 0
-
-    puts("group=#{group}, channel=#{channel}, timer=#{timer}, resolution=#{resolution}, f=#{f}, d=#{d}")
 
     ESP32::LEDC.timer_config(group, timer, resolution, f)
     ESP32::LEDC.channel_config(pin, group, timer, channel)
